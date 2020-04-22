68aa325eca16560f02ac650e252f730b4fb30f8f
SolomiiaNakonechna <snako@softserveinc.com>
2020-04-22 19:50:05 +0300

    Create Entity Fields and ChoiceOptions

---
+++ src/main/java/com/ss/retraining/Entity/Fields.java
@@ -0,0 +1,56 @@
+package com.ss.retraining.Entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "fields")
+public class Fields {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "field_type")
+    private int fieldType;
+
+    @Column(name = "is_strict")
+    private Boolean isStrict;
+
+    @Column(name = "created")
+    private Timestamp created;
+
+    @OneToMany(mappedBy = "formEntity")
+    private List<FormFields> formFields;
+
+    @OneToMany(mappedBy = "fields")
+    private List<ChoiceOptions> choiceOptions;
+
+    @OneToMany(mappedBy = "fieldsEntity")
+    private List<SharedFields> sharedFields;
+
+    @OneToMany(mappedBy = "field_id")
+    private List<SettingsAutocomplete> settingsAutocomplete;
+
+    @ManyToMany
+    @JoinTable(name = "field_range",
+            joinColumns = @JoinColumn(name = "id"),
+            inverseJoinColumns = @JoinColumn(name = "field_id"))
+    private List<Fields> fields;
+
+    @ManyToOne
+    @JoinColumn(name = "id")
+    private Users owners;
+}
+
