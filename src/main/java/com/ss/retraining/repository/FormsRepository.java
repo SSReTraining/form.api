390f3cd71e24f5a2575a5b92157811f64d2de2df
annakitchak <annakitchak@gmail.com>
2020-04-22 20:11:04 +0300

    added entity and repository

---
+++ src/main/java/com/ss/retraining/repository/FormsRepository.java
@@ -0,0 +1,10 @@
+package com.ss.retraining.repository;
+
+import com.ss.retraining.entity.Forms;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by annakitchak on 22.04.20.
+ */
+public interface FormsRepository extends JpaRepository<Forms, Long> {
+}
