96cd54e2e97c8b49ea9fb17614b259ee311e65bf
P_Boian <pavloboianovskii@gmail.com>
2020-04-23 12:20:03 +0300

    Creating entity users

--- src/main/java/com/ss/retraining/RetrainingApplication.java
+++ src/main/java/com/ss/retraining/RetrainingApplication.java
@@ -5,11 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RetrainingApplication {
-
-
-	public static void main(String[] args) {
-		SpringApplication.run(RetrainingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RetrainingApplication.class, args);
+    }
 
 }
-ё
