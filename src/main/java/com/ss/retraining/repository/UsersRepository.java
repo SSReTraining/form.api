6be609a96d46224ef4988449537ff0a2539ed549
P_Boian <pavloboianovskii@gmail.com>
2020-04-24 00:30:56 +0300

    Commit adding UsersDTO, UsersService, UsersServiceImpl

--- src/main/java/com/ss/retraining/repository/UsersRepository.java
+++ src/main/java/com/ss/retraining/repository/UsersRepository.java
@@ -3,6 +3,6 @@ package com.ss.retraining.repository;
 import com.ss.retraining.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<Users, Long> {
+public interface UsersRepository extends JpaRepository<Users, Long> {
 
 }
