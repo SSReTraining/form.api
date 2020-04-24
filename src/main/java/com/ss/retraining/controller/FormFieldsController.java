3af5ec2cbee537fcbcb9176d9090efe0db410167
wasssabi <nazarlucholat@gmail.com>
2020-04-24 16:04:28 +0300

    relationship fix

---
+++ src/main/java/com/ss/retraining/controller/FormFieldsController.java
@@ -0,0 +1,32 @@
+package com.ss.retraining.controller;
+
+import com.ss.retraining.dto.FormFieldsDTO;
+import com.ss.retraining.service.FormFieldsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/formfields")
+public class FormFieldsController {
+    @Autowired
+    private FormFieldsService formFieldsService;
+
+    @GetMapping("/all")
+    public List<FormFieldsDTO> getAllFormFields() {
+        return formFieldsService.getAllFormFields();
+
+    }
+
+    @GetMapping("/{id}")
+    public FormFieldsDTO getFormFieldsById(@PathVariable("id") Long id ){
+        return formFieldsService.getByFormFieldsId(id);
+    }
+
+    @PostMapping
+    public void createFormFields(@RequestBody FormFieldsDTO formFieldsDTO){
+        formFieldsService.createFormFields(formFieldsDTO);
+
+    }
+}
