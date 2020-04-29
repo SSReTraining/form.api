db86ffccd789c98ef97531420321836de4eb4717
P_Boian <pavloboianovskii@gmail.com>
2020-04-29 06:25:24 +0300

    Adding basic registration

--- src/main/java/com/ss/retraining/repository/UsersRepository.java
+++ src/main/java/com/ss/retraining/repository/UsersRepository.java
@@ -2,7 +2,9 @@ package com.ss.retraining.repository;
 
 import com.ss.retraining.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsersRepository extends JpaRepository<Users, Long> {
 
 }
